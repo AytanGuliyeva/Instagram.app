0a63cd85b263e821f2ac69ef93c66aff198decd7
AytanGuliyeva <aytnguliyeva@gmail.com>
2024-02-03 20:08:32 +0400

    Main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.instagramapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.instagramapp"
