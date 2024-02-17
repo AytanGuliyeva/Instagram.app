c41ea7aacd44b5713e611497f9ee8a759a28ab92
AytanGuliyeva <aytnguliyeva@gmail.com>
2024-02-17 18:10:08 +0400

    Icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,11 @@ android {
 
 dependencies {
 
+    //navigation component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
--- app/src/main/java/com/example/instagramapp/authorization/LoginFragment.kt
+++ app/src/main/java/com/example/instagramapp/authorization/LoginFragment.kt
@@ -1,10 +1,11 @@
-package com.example.instagramapp
+package com.example.instagramapp.authorization
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.instagramapp.R
 
 class LoginFragment : Fragment() {
 
